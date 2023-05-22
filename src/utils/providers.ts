89e42c2094086ce4fb0d7c45a17827778067742d
JD <jann.driessen@gmx.de>
2023-05-22 11:04:05 +0200

    refactor: add provider utils

---
+++ src/utils/providers.ts
@@ -0,0 +1,23 @@
+import { providers } from "ethers"
+
+function getAlchemySubdomain(chainId: number): string | null {
+  switch (chainId) {
+    case 1:
+      return "eth"
+    case 10:
+      return "opt"
+    case 137:
+      return "polygon"
+    default:
+      return null
+  }
+}
+
+export function buildAlchemyProvider(
+  chainId: number,
+  apiKey: string,
+): providers.JsonRpcProvider {
+  const subdomain = getAlchemySubdomain(chainId)
+  const url = `https://${subdomain}-mainnet.g.alchemy.com/v2/${apiKey}`
+  return new providers.JsonRpcProvider(url)
+}
