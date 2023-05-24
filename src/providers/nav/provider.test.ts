18d10daae1f568672fcc86fd2135062e0fc97505
JD <jann.driessen@gmx.de>
2023-05-24 10:56:26 +0200

    feat: add nav provider

--- src/providers/nav/provider.test.ts
+++ src/providers/nav/provider.test.ts
@@ -1,3 +1,5 @@
+import { BigNumber, Contract, providers, utils } from "ethers"
+
 import { buildAlchemyProvider, CoinGeckoService } from "../../utils"
 import { IndexNavProvider } from "./provider"
 
@@ -6,10 +8,59 @@ const coingeckoService = new CoinGeckoService(process.env.COINGECKO_API_KEY!)
 const rpcProvider = buildAlchemyProvider(1, process.env.ALCHEMY_API_KEY!)
 
 describe("IndexSupplyProvider", () => {
-  test("it returns the supply for icSMMT", async () => {
+  test("returns the supply for icSMMT", async () => {
     const icSMMT = "0xc30FBa978743a43E736fc32FBeEd364b8A2039cD"
     const provider = new IndexNavProvider(rpcProvider, coingeckoService)
     const nav = await provider.getNav(icSMMT)
     await expect(nav).toBeGreaterThan(0)
   })
+
+  test("returns the supply for icETH", async () => {
+    const icETH = "0x7C07F7aBe10CE8e33DC6C5aD68FE033085256A84"
+    const expectedNav = await getNav(icETH, 1)
+    const provider = new IndexNavProvider(rpcProvider, coingeckoService)
+    const nav = await provider.getNav(icETH)
+    await expect(nav).toBeCloseTo(expectedNav)
+  })
 })
+
+async function getNav(address: string, chainId: number) {
+  const contract = new Contract(address, indexTokenAbi, rpcProvider)
+  const positions = await contract.getPositions()
+  const positionPrices = await coingeckoService.findPrices({
+    addresses: positions.map((p: any) => p.component),
+    chainId,
+    baseCurrency: "usd",
+    includeDailyChange: false,
+  })
+  const usdValues = positions.map((p: any, index: number) => {
+    const priceUsd = positionPrices[p.component.toLowerCase()]?.["usd"]
+    const unit = utils.formatUnits(p.unit.toString())
+    return priceUsd * Number(unit)
+  })
+  const nav = usdValues.reduce((prev: number, curr: number) => curr + prev, 0)
+  return nav
+}
+
+const indexTokenAbi = [
+  {
+    inputs: [],
+    name: "getPositions",
+    outputs: [
+      {
+        components: [
+          { internalType: "address", name: "component", type: "address" },
+          { internalType: "address", name: "module", type: "address" },
+          { internalType: "int256", name: "unit", type: "int256" },
+          { internalType: "uint8", name: "positionState", type: "uint8" },
+          { internalType: "bytes", name: "data", type: "bytes" },
+        ],
+        internalType: "struct ISetToken.Position[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+]
--- src/providers/nav/provider.ts
+++ src/providers/nav/provider.ts
@@ -1,15 +1,21 @@
-import { providers } from "ethers"
+import { BigNumber, Contract, providers, utils } from "ethers"
 
-import { CoinGeckoService } from "utils/coingecko"
+import { CoinGeckoService, FindPricesResponse } from "utils/coingecko"
 import { IcSmmtNavProvider } from "./icsmmt"
 
 interface NavProvider {
   getNav(address: string): Promise<number>
 }
 
+interface Position {
+  component: string
+  unit: BigNumber
+}
+
 const icSMMT = "0xc30FBa978743a43E736fc32FBeEd364b8A2039cD"
 
 export class IndexNavProvider implements NavProvider {
+  private baseCurrency = "usd"
   constructor(
     private readonly provider:
       | providers.JsonRpcProvider
@@ -18,12 +24,60 @@ export class IndexNavProvider implements NavProvider {
   ) {}
 
   async getNav(address: string): Promise<number> {
-    const { coingeckoService, provider } = this
+    const { baseCurrency, coingeckoService, provider } = this
+    const network = await provider.getNetwork()
+    const chainId = network.chainId
+    // icSMMT specific calculation
     if (address.toLowerCase() === icSMMT.toLowerCase()) {
       const icSmmtProvider = new IcSmmtNavProvider(provider, coingeckoService)
       const nav = await icSmmtProvider.getNav()
       return nav
     }
-    return 0
+    // Any other Index token
+    const contract = new Contract(address, indexTokenAbi, provider)
+    const positions: Position[] = await contract.getPositions()
+    const positionPrices = await this.getPositionPrices(positions, chainId)
+    const usdValues = positions.map((p: Position, index: number) => {
+      const priceUsd = positionPrices[p.component.toLowerCase()]?.[baseCurrency]
+      const unit = utils.formatUnits(p.unit.toString())
+      return priceUsd * Number(unit)
+    })
+    const nav = usdValues.reduce((prev, curr) => curr + prev, 0)
+    return nav
+  }
+
+  private async getPositionPrices(
+    positions: Position[],
+    chainId: number,
+  ): Promise<FindPricesResponse> {
+    return await this.coingeckoService.findPrices({
+      addresses: positions.map((p) => p.component),
+      chainId,
+      baseCurrency: this.baseCurrency,
+      includeDailyChange: false,
+    })
   }
 }
+
+const indexTokenAbi = [
+  {
+    inputs: [],
+    name: "getPositions",
+    outputs: [
+      {
+        components: [
+          { internalType: "address", name: "component", type: "address" },
+          { internalType: "address", name: "module", type: "address" },
+          { internalType: "int256", name: "unit", type: "int256" },
+          { internalType: "uint8", name: "positionState", type: "uint8" },
+          { internalType: "bytes", name: "data", type: "bytes" },
+        ],
+        internalType: "struct ISetToken.Position[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+]
