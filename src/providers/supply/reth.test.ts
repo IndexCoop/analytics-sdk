a5acb5425f6b0274a506a527e43d9ffaa17323ab
JD <jann.driessen@gmx.de>
2023-06-20 09:55:50 +0200

    chore: add new convenience provider

--- src/providers/supply/reth.test.ts
+++ src/providers/supply/reth.test.ts
@@ -1,10 +1,9 @@
 import { BigNumber, Contract, utils } from "ethers"
 
-import { buildAlchemyProvider } from "../../utils"
+import { buildAlchemyProviderPredefined } from "../../utils"
 import { IndexREthProvider } from "./reth"
 
-/* eslint-disable  @typescript-eslint/no-non-null-assertion */
-const rpcProvider = buildAlchemyProvider(1, process.env.ALCHEMY_API_KEY!)
+const rpcProvider = buildAlchemyProviderPredefined(1)
 
 const AaveProtocolDataProvider = "0x7B4EB56E7CD4b454BA8ff71E4518426369a138a3"
 const rETH = "0xae78736Cd615f374D3085123A210448E74Fc6393"
--- src/utils/providers.ts
+++ src/utils/providers.ts
@@ -21,3 +21,13 @@ export function buildAlchemyProvider(
   const url = `https://${subdomain}-mainnet.g.alchemy.com/v2/${apiKey}`
   return new providers.JsonRpcProvider(url)
 }
+
+export function buildAlchemyProviderPredefined(
+  chainId: number,
+): providers.JsonRpcProvider {
+  const subdomain = getAlchemySubdomain(chainId)
+  /* eslint-disable  @typescript-eslint/no-non-null-assertion */
+  const url = `https://${subdomain}-mainnet.g.alchemy.com/v2/${process.env
+    .ALCHEMY_API_KEY!}`
+  return new providers.JsonRpcProvider(url)
+}
