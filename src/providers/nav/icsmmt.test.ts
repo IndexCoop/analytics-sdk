391a2430fee6dde8732fcfbcfdbe3646d7ec9813
JD <jann.driessen@gmx.de>
2023-07-27 16:53:16 +0200

    test: clean up

--- src/providers/nav/icsmmt.test.ts
+++ src/providers/nav/icsmmt.test.ts
@@ -1,12 +1,12 @@
 import { buildAlchemyProvider, CoinGeckoService } from "../../utils"
 import { IcSmmtNavProvider } from "./icsmmt"
 
-/* eslint-disable  @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 const coingeckoService = new CoinGeckoService(process.env.COINGECKO_API_KEY!)
 const rpcProvider = buildAlchemyProvider(1, process.env.ALCHEMY_API_KEY!)
 
 describe("IcSmmtNavProvder", () => {
-  test("returns supply for icSMMT", async () => {
+  test("returns NAV for icSMMT", async () => {
     const provider = new IcSmmtNavProvider(rpcProvider, coingeckoService)
     const nav = await provider.getNav()
     expect(nav).toBeGreaterThan(0)
