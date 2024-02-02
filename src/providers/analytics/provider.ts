7866410f31e7ef534b0c42ea10aca60e3f698c9c
JD <jann.driessen@gmx.de>
2024-02-02 12:12:28 +0100

    add static token data

--- src/providers/analytics/provider.ts
+++ src/providers/analytics/provider.ts
@@ -1,9 +1,11 @@
-import { Contract, providers, utils } from "ethers"
+import { providers, utils } from "ethers"
 
 import { CoinGeckoService, CoinGeckoUtils } from "../../utils"
 import { IndexMarketCapProvider } from "../marketcap"
 import { IndexNavProvider } from "../nav"
 import { IndexSupplyProvider } from "../supply"
+
+import { TokenData } from "./token-data"
 
 interface IndexAnalytics {
   name: string
@@ -22,13 +24,6 @@ interface AnalyticsProvider {
   getAnalytics(address: string): Promise<IndexAnalytics>
 }
 
-const Erc20Abi = [
-  "function decimals() public view returns (uint8)",
-  "function name() public view returns (string memory)",
-  "function symbol() public view returns (string memory)",
-  "function totalSupply() public view returns (uint256)",
-]
-
 export class IndexAnalyticsProvider implements AnalyticsProvider {
   private baseCurrency = "usd"
 
@@ -41,18 +36,16 @@ export class IndexAnalyticsProvider implements AnalyticsProvider {
 
   async getAnalytics(address: string): Promise<IndexAnalytics> {
     const { baseCurrency, coingeckoService, provider } = this
-    const contract = new Contract(address, Erc20Abi, this.provider)
     const marketCapProvider = new IndexMarketCapProvider(
       provider,
       coingeckoService,
     )
     const navProvider = new IndexNavProvider(provider, coingeckoService)
     const supplyProvider = new IndexSupplyProvider(provider)
-    const network = await provider.getNetwork()
-    const chainId = network.chainId
-    const name = await contract.name()
-    const symbol = await contract.symbol()
-    const decimals = await contract.decimals()
+    // For now we can save time by always assuming Ethereum
+    const chainId = 1
+    // Token data fetched statically to avoid lengthy contract interactions
+    const token = TokenData[address.toLowerCase()]
     const totalSupply = await supplyProvider.getSupply(address)
     const coingeckoRes = await coingeckoService.getTokenPrice({
       address,
@@ -74,10 +67,10 @@ export class IndexAnalyticsProvider implements AnalyticsProvider {
     const navPrice = await navProvider.getNav(address)
     const supplyFormatted = utils.formatUnits(totalSupply.toString())
     return {
-      symbol,
+      symbol: token.symbol,
       address,
-      name,
-      decimals,
+      name: token.name,
+      decimals: token.decimals,
       totalSupply: supplyFormatted,
       marketPrice,
       navPrice,
