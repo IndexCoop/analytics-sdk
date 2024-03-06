48ed462f0b1cdb8f7e41a2c69392df1411ee25d1
JD <jann.driessen@gmx.de>
2024-03-06 11:24:04 +0000

    chore: add mappings for new ic21 components

--- src/providers/nav/ic21.ts
+++ src/providers/nav/ic21.ts
@@ -11,12 +11,14 @@ type Position = {
 const Erc20Abi = ["function decimals() public view returns (uint8)"]
 
 const mappingAddressCoingeckoId: { [key: string]: string } = {
+  "0x399508A43d7E2b4451cd344633108b4d84b33B03": "avalanche-2",
   "0x3f67093dfFD4F0aF4f2918703C92B60ACB7AD78b": "bitcoin",
   "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2": "ethereum",
   "0x1bE9d03BfC211D83CFf3ABDb94A75F9Db46e1334": "binancecoin",
   "0x0d3bd40758dF4F79aaD316707FcB809CD4815Ffe": "ripple",
   "0x9c05d54645306d4C4EAd6f75846000E1554c0360": "cardano",
   "0xb80a1d87654BEf7aD8eB6BBDa3d2309E31D4e598": "solana",
+  "0x514910771AF9Ca656af840dff83E8264EcF986CA": "chainlink",
   "0x9F2825333aa7bC2C98c061924871B6C016e385F3": "litecoin",
   "0xF4ACCD20bFED4dFFe06d4C85A7f9924b1d5dA819": "polkadot",
   "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0": "matic-network",
