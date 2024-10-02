a83980fb0b9d6daa4889e52bb86b019124b8991c
JD <jann.driessen@gmx.de>
2024-10-02 11:17:54 +0100

    improve getting fli components

--- src/providers/nav/fli-nav-provider.ts
+++ src/providers/nav/fli-nav-provider.ts
@@ -1,6 +1,9 @@
 import { providers, utils } from "ethers"
 
-import { isAddressEqual, tokenSymbolMap } from "@nsorcell/exp-tokenlist"
+import {
+  getTokenByChainAndAddress,
+  getTokenByChainAndSymbol,
+} from "@nsorcell/exp-tokenlist"
 
 import { CoinGeckoService } from "utils/coingecko"
 import { getPositions } from "../../utils/positions"
@@ -18,13 +21,13 @@ export class FliNavProvider {
   async getNav(address: string): Promise<number> {
     const { coingeckoService, provider } = this
     const positions = await getPositions(address, provider)
-    const eth2xfliAddress = tokenSymbolMap[1]["ETH2x-FLI"].address
-    const isEth2xFli = isAddressEqual(address, eth2xfliAddress)
-    const component = isEth2xFli
-      ? tokenSymbolMap[1]["ETH2X"].address
-      : tokenSymbolMap[1]["BTC2X"].address
+    const componentSymbol =
+      getTokenByChainAndAddress(1, address)?.symbol === "ETH2x-FLI"
+        ? "ETH2X"
+        : "BTC2X"
+    const component = getTokenByChainAndSymbol(1, componentSymbol)
     const navProvider = new IndexNavProvider(provider, coingeckoService)
-    const fliNav = await navProvider.getNav(component)
+    const fliNav = await navProvider.getNav(component.address)
     const unit = utils.formatUnits(positions[0].unit.toString(), 18)
     const nav = Number(unit) * fliNav
     return nav
